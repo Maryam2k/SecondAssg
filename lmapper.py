49f0ce47354c8d09b77284e3fdff40c1f7db4dcd
mdavoodi@emich.edu <mdavoodi@emich.edu>
2016-03-06 15:22:06 -0500

    2

---
+++ lmapper.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys
+# import re
+my_list = ["_","~","!","'","@","#","$","%","^","&","*","(",")",'"',"-","=","+","/",">",".","?","|","<",",","`",":","}",";","{","\/"]
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    for i in my_list:
+        line  = line.replace(i,'')
+
+    words = line.split()
+    
+    # increase counters
+    for word in words:        
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print '%s\t%s' % (len(word), word)
