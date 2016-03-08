3bc30cd5799bd6aa3813e950b346a0b8f0e48d82
mdavoodi@emich.edu <mdavoodi@emich.edu>
2016-03-08 16:56:43 -0500

    2

--- lmapper.py
+++ lmapper.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 
-# import sys
-# my_list = ["_","~","!","'","@","#","$","%","^","&","*","(",")",'"',"-","=","+","/",">",".","?","|","<",",","`",":","}",";","{","\/"]
-
-# for line in sys.stdin:
-#     line = line.strip()
-
-
-#     # new_string = re.sub('[!@#${}.;\d+:,~\]+)\(?\[&\"*+\=\'%^/_|-]', ' ', line)
-#     words = new_string.split()
-       
-#     for word in words:        
- 
-#         print '%s\t%s' % (len(word), word)
 import sys
 import re
-# my_list = ["_","~","!","'","@","#","$","%","^","&","*","(",")",'"',"-","=","+","/",">",".","?","|","<",",","`",":","}",";","{","\/"]
 for line in sys.stdin:
     line = line.strip()
     new_string = re.sub('[^A-Za-z0-9]+',' ', line)
 
+
     words = new_string.split()
     for word in words:    
-        print '%s\t%s' % (len(word), 1)
+        print '%s\t%s' % (len(word), word)
--- lreducer.py
+++ lreducer.py
@@ -1,133 +1,26 @@
 #!/usr/bin/env python
-
-# from operator import itemgetter
-# import sys
-
-# current_length = None
-# current_count = 0
-# word = None
-# for line in sys.stdin:
-#     line = line.strip()
-
-#     len_word, word = line.split('\t', 1)
-#     if current_length == len_word:
-#         current_count += 1
-#     else:
-#         if current_length:
-#             # write result to STDOUT
-#             print '%s\t%s' % (current_length, current_count)
-#         current_count = 1
-#         current_length = len_word
-
-# if current_length == len_word:
-#     print '%s\t%s' % (current_length, current_count)
-
-
 from operator import itemgetter
-import sys,re
+import sys
 
-current_word = None
+current_length = None
 current_count = 0
 word = None
-
-# input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
 
-    # parse the input we got from mapper.py
-    word, count = line.split("\t", 1)
-
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
+    len_word, word = line.split('\t', 1)
+    if current_length == len_word:
+        current_count += 1
     else:
-        if current_word:
+        if current_length:
             # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+            print '%s\t%s' % (current_length, current_count)
+        current_count = 1
+        current_length = len_word
 
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
-
+if current_length == len_word:
+    print '%s\t%s' % (current_length, current_count)
 
 
 
 
-# from operator import itemgetter
-# import sys
-
-
-# current_length = None
-# current_count = 0
-# word = None
-# allWords = []
-
-# for line in sys.stdin:
-#     line = line.strip()
-
-#     len_word, word = line.split('\t', 1)
-#     if current_length == len_word:
-#         current_count += 1
-#         allWords.append(word)
-
-
-#     else:
-#         if current_length:
-#             # write result to STDOUT
-#             print '%s\t%s' % (current_length, allWords)
-#         current_count = 1
-#         current_length = len_word
-
-# if current_length == len_word:
-#     print '%s\t%s' % (current_length, allWords)
-
-# ************************************
-# from operator import itemgetter
-# import sys,re
-
-# current_word = None
-# current_count = 0
-# word = None
-
-# # input comes from STDIN
-# for line in sys.stdin:
-#     # remove leading and trailing whitespace
-#     line = line.strip()
-
-#     # parse the input we got from mapper.py
-#     word, count = line.split("\t", 1)
-
-#     # convert count (currently a string) to int
-#     try:
-#         count = int(count)
-#     except ValueError:
-#         # count was not a number, so silently
-#         # ignore/discard this line
-#         continue
-
-#     # this IF-switch only works because Hadoop sorts map output
-#     # by key (here: word) before it is passed to the reducer
-#     if current_word == word:
-#         current_count += count
-#     else:
-#         if current_word:
-#             # write result to STDOUT
-#             print '%s\t%s' % (current_word, current_count)
-#         current_count = count
-#         current_word = word
-
-# # do not forget to output the last word if needed!
-# if current_word == word:
-#     print '%s\t%s' % (current_word, current_count)
