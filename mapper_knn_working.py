7919f31a2cad9d4473303c47bee179e81c133a5d
mdavoodi@emich.edu <mdavoodi@emich.edu>
2016-04-17 10:30:07 -0400

    modified

--- mapper_knn_working.py
+++ mapper_knn_working.py
@@ -2,38 +2,20 @@
 
 import sys
 import re
-import os, math
+import os
 
-attribute_count = 784 
+attribute_count = 784
 #in mapper key is a test data. Value is traing data+it's label
 for line in sys.stdin:
     #print os.environ["map.input.file"]
-    #print line
+
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
     line = line.split()
-    
-    #test[0:783] [784][785]
-    test_record = line[0:attribute_count]
-    test_label = line[attribute_count]
-
-    train_record = line[attribute_count+1:2*attribute_count+1]
-    train_label = line[2*attribute_count+1]
-
-    
-    #print test_label,' ', train_label
-
-    distance = 0
-    for i in range(0,attribute_count):
-            distance += math.pow(int(test_record[i]) - int(train_record[i]), 2)
-    distance=math.sqrt(distance)
-    distance = str(round(distance,2))
-
-    
-    test_record = ' '.join(test_record)
-
-    print '%s\t%s' % (test_record+" "+test_label, distance+" "+train_label)
+    input_key = ' '.join(line[0:attribute_count+1])
+    input_value = ' '.join(line[attribute_count+1:])
+    print '%s\t%s' % (input_key, input_value)
 
 
 
@@ -42,5 +24,3 @@ for line in sys.stdin:
 
 #large file
 #cat  /Users/mrym/Desktop/image_full_combined_small.txt | python /Users/mrym/Desktop/bigData/SecondAssg/mapper_knn.py |sort -k1,1 | python /Users/mrym/Desktop/bigData/SecondAssg/reducer_knn.py
-
-
--- reducer_knn_working.py
+++ reducer_knn_working.py
@@ -16,33 +16,46 @@ import operator
 #distance is the distance to the test data
 def classify(test_record, train_list):
 
-    #distances = []
+    distances = []
     class_freq = [0]*10
-
-
-    train_list = [(float(x[0]),x[1]) for x in train_list]
-    #print train_list
-    #sort the training records based on distance 0:783(attributes),784(class), 785(distance) 
+    #print 'train_list', train_list
+    #computing distances and store them into the list distances
+    for train_record in train_list:
+        distance = 0
+        for i in range(0,attribute_count):
+            distance += math.pow(int(test_record[i]) - int(train_record[i]), 2)
+        distance = round(math.sqrt(distance),2)
+        #print distance
+        #print test_record, train_record, distance
+        distances.append(distance)
 
    
-    train_list = sorted(train_list, key=operator.itemgetter(0))  
 
+    # join the training_list and distances    
+    for index in range(0,len(train_list)):
+        train_list[index].append(distances[index])
+
+    #sort the training records based on distance 0:783(attributes),784(class), 785(distance) 
+    train_list = sorted(train_list, key=operator.itemgetter(attribute_count+1))  
+
+    #print train_list
     #the closest 
     train_list = train_list[0:k]
+
+    #print train_list
+
     # k is the number of nearest neghbor
-
-    #print train_list    
-
+    
     # find the majority class
     for i in range(0,len(train_list)):
         # label of the ith training record from k closset one=
         # int(train_list[i][attribute_count])
-        class_freq[int(train_list[i][1])] +=1/(float(train_list[i][0])+0.1)
+        class_freq[int(train_list[i][attribute_count])] +=1/(float(train_list[i][attribute_count+1])+0.1)
 
     #print class_freq
+
     max_value = max(class_freq)
     max_index = class_freq.index(max_value)
-
 
     return max_index
 
@@ -52,17 +65,17 @@ prior_test_record = None
 test_record = None
 train_record = None
 k=4
-attribute_count = 784 
+attribute_count = 784   
 
 for line in sys.stdin:
 
     # remove leading and trailing whitespace
     line = line.strip()
-    #print line
+
     # parse the input we got from mapper.py
     test_record, train_record = line.split("\t", 1)
-    train_record_splitted = train_record.rstrip().split(' ')
-
+    train_record_splitted = train_record.split(' ')
+        
     if prior_test_record == None: #first-line
         prior_test_record = test_record
 
@@ -76,13 +89,10 @@ for line in sys.stdin:
         print '%s\t%s' % (actual_label,predicted_label) 
 
         train_list = []
-        #print '%s\t%s' % (prior_test_record,predicted_label)
         prior_test_record = test_record
 
-#print 'hey', test_record
 
 test_record_splitted= test_record.rstrip().split(' ')      
 predicted_label = classify(test_record_splitted[0:attribute_count], train_list)
 actual_label = test_record_splitted[attribute_count]
 print '%s\t%s' % (actual_label,predicted_label)
-
