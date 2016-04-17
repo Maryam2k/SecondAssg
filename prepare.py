8ea03c029d147aec25d8a01c0314427d56654ff2
mdavoodi@emich.edu <mdavoodi@emich.edu>
2016-04-17 00:34:20 -0400

    m

--- prepare.py
+++ prepare.py
@@ -6,8 +6,8 @@ ftest = open("image_test_full.txt", 'r')
 # 784 bif without label
 fcombined = open("image_full_combined_small.txt", 'w')
 # each test data connects to each train data.
-train_records = ftrain.readlines()[0:sys.argv[1]]
-test_records = ftest.readlines()[0:sys.argv[2]]
+train_records = ftrain.readlines()[0:int(sys.argv[1])]
+test_records = ftest.readlines()[0:int(sys.argv[2])]
 
 i=0
 for item1 in test_records:
